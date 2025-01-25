772db9b6b72c82eda0252b0b30528b6cad85ab0f
demetre151 <tsetskhladzedemetre5@gmail.com>
2025-01-25 18:20:40 +0300

    commit

---
+++ level 33/classwork/class.py
@@ -0,0 +1,17 @@
+რაც_მე_მიყვარს="lomi"
+print(f"მე მიყვარს {რაც_მე_მიყვარს} {10+10}")
+
+print("{1} arid kargi biwi".format("kata","lomi"))
+name="deme"
+print(f"{name} aris lomi")
+print("{x}".format(x= 10+ 10 , y=20))
+print(f"{10+10}")
+name_of_words="davit","demetre","vaja","guga","gio"
+print(f"{name_of_words[0]} {name_of_words[1]}")
+joined_name_of_words=",".join(name_of_words)
+print(joined_name_of_words)
+joined_name=" ".join(name)
+print(joined_name)
+x ="DXDXD"
+res= x.split("D")
+print(res)
---
+++ level 33/classwork/class2.py
@@ -0,0 +1,6 @@
+words = "demetre"
+print(f"chemi saxelia {words}")
+#homewok 2
+res = words.split("e")
+print(res)
+splited_words=",".join(words)
